19a213fdbc5734adf6376736c8976795925f4bc7
ArielGuevara <adguevara7@espe.edu.ec>
2025-11-10 12:30:02 -0500

    new Character, configurate the physics

--- esp-erancito/Scenes/Characteres/boy_1.gd
+++ esp-erancito/Scenes/Characteres/boy_1.gd
@@ -4,10 +4,12 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var appeared:bool = false
+var attacking:bool = false
 
 
 func _ready():
 	$animaciones.play("appearing")
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,6 +19,12 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+		# Detectar ataque
+	if Input.is_action_just_pressed("attack") and not attacking:
+		attacking = true
+		$animaciones.play("attack_1")
+		return  # No seguimos con el resto para no sobreescribir animación
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,32 +33,35 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
-
 	decide_animation()
+	#_on_animaciones_animation_finished()
+	
 func decide_animation():
 	if not appeared: return
-	
+	if attacking: return  
 	# Eje de las x
 	if velocity.x == 0:
 		$animaciones.play("idle")
 	elif velocity.x < 0:
 		$animaciones.flip_h = true
-		$animaciones.play("run")
+		$animaciones.play("walk")
 	elif velocity.x > 0:
 		$animaciones.flip_h = false
-		$animaciones.play("run")
+		$animaciones.play("walk")
 		
 	#Eje de las Y
 	if velocity.y > 0:
-		$animaciones.play("fall")
+		$animaciones.play("idle")
 	elif velocity.y < 0:
-		$animaciones.play("jump_up")
+		$animaciones.play("jump")
 		
 
-
 func _on_animaciones_animation_finished() -> void:
-	print($animaciones.animation)
-	if $animaciones.animation == "appearing":
+	
+	if $animaciones.animation == "idle":
 		appeared = true
+	if $animaciones.animation == "attack_1":
+		$animaciones.play("idle")
+		attacking = false
